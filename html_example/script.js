1f84e012823cc6c041fbbd35a0a03c2ae9bdc6c5
Sylhare <sylhareng@gmail.com>
2017-05-17 19:30:29 -0400

    Minor Corrections
    :package: :sailboat:  :baby_chick:

--- html_example/script.js
+++ html_example/script.js
@@ -1,16 +1,17 @@
 var progressB = document.getElementById("progressBar");
 var p = 1;
 
-function progress() {
-    /* to complete the progression bar, should implement a stop at some point */
-    setInterval(completion, 200);
-}
 
 function completion() {
     /* Update the progress bar completion */
     document.getElementById("progressBar").value = p; //works fine
     //progressB.value = p; //returns TypeError cannot set property type to 'null' on jsfiddle
     p++;
+}
+
+function progress() {
+    /* to complete the progression bar, should implement a stop at some point */
+    setInterval(completion, 200);
 }
 
 function allowDrop(ev) {
@@ -27,15 +28,18 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
-/**var canvas = document.getElementById('canvas1');
-        ctx =canvas.getContext('2d');
-        ctx.font="bold 22px Tahoma";
-        ctx.textAlign="start";
-        ctx.fillText("start", 10, 30);
-        ctx.translate(100, 150);
-        ctx.fillText("after translate", 0, 0);
-        ctx.rotate(1);
-        ctx.fillText("after rotate", 0, 0);
-        ctx.scale(1.5, 4);
-        ctx.fillText("after scale", 0,20);
+/*
+
+var canvas = document.getElementById('canvas1');
+ctx =canvas.getContext('2d');
+ctx.font="bold 22px Tahoma";
+ctx.textAlign="start";
+ctx.fillText("start", 10, 30);
+ctx.translate(100, 150);
+ctx.fillText("after translate", 0, 0);
+ctx.rotate(1);
+ctx.fillText("after rotate", 0, 0);
+ctx.scale(1.5, 4);
+ctx.fillText("after scale", 0,20);
+
 */
--- tests/CanvasCreation.js
+++ tests/CanvasCreation.js
@@ -1,5 +1,5 @@
 var body = document.getElementsByTagName("body")[0];
-var canvas = document.createElement('canvas');
+var canvas = document.createElement("canvas");
 canvas.style = "border:10px solid #d3d3d3;";
 
 body.appendChild(canvas);
