3666f1acbc68156486a5b2d037b02430f9e4a108
sylhare <sylhareng@gmail.com>
2017-07-11 16:36:06 -0400

    Appcache example
    Added an example on appcache

---
+++ ex_Javascript/appcache/script.js
@@ -0,0 +1,18 @@
+//https://developer.mozilla.org/fr/docs/Web/HTML/Utiliser_Application_Cache
+//https://www.html5rocks.com/en/tutorials/appcache/beginner/
+
+function onUpdateReady() {
+    console.log('nouvelle version trouvée !');
+}
+
+window.applicationCache.addEventListener('updateready', onUpdateReady);
+
+if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+    onUpdateReady();
+}
+
+window.applicationCache.update()
+
+var appCache = window.applicationCache;
+console.log(cache);
+
