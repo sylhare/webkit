0956925799e701465a9d8d0293a1a6db23dffe80
sylhare <sylhareng@gmail.com>
2018-02-16 15:45:38 -0500

    Adding DOM manipulation basic testing

--- src/ex_Javascript/javascript_test/tests_jasmine.js
+++ src/ex_Javascript/javascript_test/tests_jasmine.js
@@ -20,3 +20,26 @@ describe("isPalindrome", () => {
         expect(isPalindrome(1001)).toEqual(true);
     });
 });
+
+
+add_elem = function (options) {
+    var para = document.createElement("p");
+    var node = document.createTextNode("This is new.");
+    para.appendChild(node);
+    var element = document.body;
+    element.appendChild(para);
+  };
+
+describe("dom manipulation", function (){
+
+  afterEach(function (){
+      var body = document.body;
+      body.removeChild(body.lastElementChild);
+  });
+
+  it("should add an element", function (){
+    add_elem();
+    expect(document.body.children.length).toEqual(2);
+    });
+    
+});
