53c02bc75a338ea22bf3d7898901ed4d88b2cdfe
Sylhare <sylhareng@gmail.com>
2018-03-03 19:22:36 -0500

    Adding colorlog

---
+++ src/ex_Javascript/colorlog.js
@@ -0,0 +1,20 @@
+function colorLog(message, color) {
+    color = color || "black";
+    switch (color) {
+        case "success":
+            color = "Green";
+            break;
+        case "info":
+            color = "DodgerBlue";
+            break;
+        case "error":
+            color = "Red";
+            break;
+        case "warning":
+            color = "Orange";
+            break;
+        default:
+            color = color;
+    }
+    console.log("%c" + message, "color:" + color);
+}
